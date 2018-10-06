16dda82e50ffb9df24e00bf603f656f6630a52d4
Bhavya Bansal <bbansal@ncsu.edu>
2018-10-06 16:42:01 -0400

    adding sign up feature

---
+++ core-bot/service.js
@@ -0,0 +1,44 @@
+var request = require('request');
+var serviceURL  = "http://service:3001";
+
+var registerationService = require('../serviceManager/registeration/registeration.js');
+
+const activityLevelMap = {1: 1.2, 2: 1.375, 3: 1.55, 4: 1.725, 5:1.9};
+module.exports = {
+
+	registerUser: function (bot, message, response, userName) {
+		console.log("*************Signing up user******************");
+    console.log(response.result.parameters);
+    // getting params from api ai response message
+		var params = {
+			"UserId": message.user,
+			"Name": userName,
+			"Weight": response.result.parameters.Weight,
+			"Height": response.result.parameters.Height,
+      "Age": response.result.parameters.Age,
+      "Gender": response.result.parameters.Gender,
+      "activityLevel": response.result.parameters.ActivityLevel,
+		};
+
+    calculateCaloriesGoal(params);
+
+    registerationService.signUpUser(params, bot, message, response);
+	}
+}
+
+function calculateCaloriesGoal(params) {
+  if (params) {
+    let weight = parseInt(params.Weight);
+    let height = parseInt(params.Height);
+    let age = parseInt(params.Age);
+    let activityLevel = parseInt(params.activityLevel);
+    let bmr = 0;
+    if (params.Gender === 'M' || params.Gender === 'male' || params.Gender === 'Male') {
+      bmr = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+    } else {
+      bmr = (10 * weight) + (6.25 * height) - (5 * age) -161;
+    }
+
+    params.caloriesGoal = bmr * activityLevelMap[activityLevel];
+  }
+}
---
+++ serviceManager/registeration/registeration.js
@@ -0,0 +1,23 @@
+
+require('../database_models/user.js');
+var mongoose = require('mongoose');
+var nock = require("nock")
+
+var User = mongoose.model('User');
+
+exports.signUpUser = function (params, bot, message, response) {
+
+    var userId = params.UserId;
+    console.log("post_keys : POST Request ")
+    console.log(userId);
+    User.findOneAndUpdate({"UserId": params.UserId, "Name": params.Name}, params, {
+        upsert: true,
+        new: true
+    }, function (err, User) {
+        if (err) {
+          bot.reply(message, "Server Error, please try again after some time")
+        } else {
+          bot.reply(message, "Registeration Succesfull");
+        }
+    });
+  };
